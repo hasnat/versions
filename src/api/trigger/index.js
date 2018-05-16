13e417d42e68ddb910f48133fab62ba89b0c6fb8
Hasnat Ullah <hasnat.ullah@gmail.com>
2018-05-16 21:27:52 +0100

    Fix sending json to ci

--- src/api/trigger/index.js
+++ src/api/trigger/index.js
@@ -6,7 +6,11 @@ const {DEPLOYMENT_TRIGGER_URI, DEPLOYMENT_TRIGGER_METHOD} = config;
 
 
 export const triggerCi = async (body) => {
-    const ciResponseBody = (await request[DEPLOYMENT_TRIGGER_METHOD](DEPLOYMENT_TRIGGER_URI).set(body)).body;
+    const ciResponseBody = (
+        await request[DEPLOYMENT_TRIGGER_METHOD](DEPLOYMENT_TRIGGER_URI)
+            .set('Content-Type', 'application/json')
+            .send(JSON.stringify(body))
+    ).body;
     console.log(ciResponseBody);
     return ciResponseBody;
 };
